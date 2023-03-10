d130ce738e9586460812ca5b6b2f69f222528885
mdinkelbach <mj.dinkelbach@mgail.com>
2023-03-10 16:16:41 -0600

    nav

---
+++ src/components/Nav.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+
+function Nav() {
+  return (
+    <ul>
+      <li>
+          About Me
+      </li>
+      <li>
+          Portfolio
+      </li>
+      <li>
+          Contact
+      </li>
+      <li>
+          Resume
+      </li>
+    </ul>
+  );
+}
+
+export default Nav;
