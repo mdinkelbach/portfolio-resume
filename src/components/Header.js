9470f6f8fcb86307a1eb14caf37f2bde6aa1a8b7
mdinkelbach <mj.dinkelbach@mgail.com>
2023-03-10 16:13:40 -0600

    header

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Nav from "./Nav";
+
+function Header() {
+  return (
+    <header className="header">
+      <h1>Mdinkelbach</h1>
+      <Nav />
+    </header>
+  );
+}
+
+export default Header;
