955c20be2c2fd557a58cfeaa55ecd6cd9df51bd3
mdinkelbach <mj.dinkelbach@mgail.com>
2023-03-13 13:52:45 -0500

    css

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import React from "react";
 import Header from "./components/Header";
 import Content from "./components/Content";
 import Footer from "./components/Footer";
+import './assets/style/App.css'
 
 function App() {
   return (
-    <div>
+    <div className="main">
       <Header />
       <Content />
       <Footer />
