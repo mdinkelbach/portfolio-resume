762b03ee352bdb0ceaf4d95e548e6d9a416ae0b2
mdinkelbach <mj.dinkelbach@mgail.com>
2023-03-13 16:09:05 -0500

    about-css

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -2,7 +2,7 @@ import profile from '../../assets/images/profile-pic.png'
 
 function About() {
   return (
-    <div>
+    <div className='about'>
         <h1>About</h1>
         <img className="profile-pic" src={profile} />
         <p className="profile-text">Hello, my name is Michael, a student of UTSA's full stack web development bootcamp. While I am familiar with front-end and back-end development, my strengths lie in back-end, particularly SQL databases. Previous to this I worked for 7 years in help desk, and have been building my own PCs since high school.</p>
