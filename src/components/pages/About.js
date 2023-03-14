4d0455059b5bdbec6cd2fd3d7432fc4ad0e1c997
mdinkelbach <mj.dinkelbach@mgail.com>
2023-03-14 12:16:27 -0500

    profile-pic

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,4 +1,4 @@
-import profile from '../../assets/images/profile-pic.png'
+import profile from '../../assets/images/profile-pic.jpg'
 
 function About() {
   return (
@@ -6,7 +6,7 @@ function About() {
         <h1>About</h1>
         <div className='profile'>
           <img className="profile-pic" src={profile} />
-          <p className="profile-text">Hello, my name is Michael, a student of UTSA's full stack web development bootcamp. While I am familiar with front-end and back-end development, my strengths lie in back-end, particularly SQL databases. Previous to this I worked for 7 years in help desk, and have been building my own PCs since high school.</p>
+          <p className="profile-text">Hello, my name is Michael, a student of UTSA's full stack web development bootcamp. Throughout the course I have become familiar with a multitude of front-end and back-end development aspects, my strengths lie in back-end. In the process, I have found that my best strengths lie in back-end, particularly SQL databases. Previous to this, I worked for 7 years in help desk, and have been building my own PCs since high school.</p>
         </div>
     </div>
   );
